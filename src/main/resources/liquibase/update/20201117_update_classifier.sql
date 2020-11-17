65a6ec0d82be7a70727f2b600f27eaa2e82c6c60
Kristjan Kruus <kristjan.kruus@ria.ee>
2020-11-17 15:31:43 +0200

    change document type name

--- src/main/resources/liquibase/update/20201117_update_classifier.sql
+++ src/main/resources/liquibase/update/20201117_update_classifier.sql
@@ -1,1 +1,1 @@
-INSERT INTO riha.classifier (id, type, code, value, discriminator) VALUES (nextval('riha.classifier_seq'), 'document_types', 'DOC_TYPE_LEGAL', 'Muu õiguslik dokumentatsioon', 'TEXT');
+INSERT INTO riha.classifier (id, type, code, value, discriminator) VALUES (nextval('riha.classifier_seq'), 'document_types', 'DOC_TYPE_LEGAL', 'Õiguslik regulatsioon', 'TEXT');
