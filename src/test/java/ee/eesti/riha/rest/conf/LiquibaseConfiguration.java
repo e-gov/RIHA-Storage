f8d5c94aa2a83428d924fc74b0eb6a861c8fddd7
Makar Sokarev <makar.shokarev@wisercat.net>
2022-04-28 15:03:26 +0300

    Revert "Adding H2 database for tests"
    This reverts commit 1a8ac646b020232bb7cc091263596043a3d665c7.

--- src/test/java/ee/eesti/riha/rest/conf/LiquibaseConfiguration.java
+++ src/test/java/ee/eesti/riha/rest/conf/LiquibaseConfiguration.java
@@ -20,7 +20,6 @@ public class LiquibaseConfiguration {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setChangeLog("classpath:db/db.changelog-test.xml");
         liquibase.setDataSource(dataSource);
-        liquibase.setDefaultSchema("riha");
         return liquibase;
     }
 }
